54740da3a4f62e8e12072365eb8fb1f4b5737428
Aaron Sikes <aaron@sikes.io>
2016-05-13 15:15:45 -0400

    Import hoodie account admin api

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('vendor/hoodie-account-client-admin.js');
+
   return app.toTree();
 };
